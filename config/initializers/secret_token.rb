30d3578ad2d45f97d3422d9455b348eccf55c6ba
Siva Pandeti <psivah@yahoo.com>
2013-02-09 13:14:41 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resta4::Application.config.secret_token = '05d0bf0b4eb4db0eaf8a7fa795fc816c9042c23df5d3a5e4f29f433bfaacdad25c239d41cc201fe526cfff4a392631ec92b6b59b8bcac18503a268e795f6fb52'
